76f3d3320c55c7f0b888ed2a2343b4da2f6b3785
123 <zlata18_00@mail.ru>
2023-09-17 21:43:28 +0300

    Задача 1 продолжение

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,15 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         task1();
+
     }
     public static void task1(){
         System.out.println("Задача 1");
         int [] arr = {1, 2, 3};
         System.out.println(Arrays.toString(arr));
+        double [] doubleArr = {1.57, 7.654, 9.986};
+        System.out.println(Arrays.toString(doubleArr));
+        long[] numbers = {987654321L};
+        System.out.println(Arrays.toString(numbers));
     }
-}
+    }
