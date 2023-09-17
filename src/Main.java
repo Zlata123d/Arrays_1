dea1cfd46c0b89566e13ec7dbce936ef767cb109
123 <zlata18_00@mail.ru>
2023-09-17 21:22:31 +0300

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+    public static void task1(){
+        System.out.println("Задача 1");
+        int [] arr = {1, 2, 3};
+        System.out.println(Arrays.toString(arr));
     }
 }
